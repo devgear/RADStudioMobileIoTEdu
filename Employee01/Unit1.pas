7794a1ac112c0d6e726c983255e75d872b3e2668
devgear <tech@embarcadero.kr>
2015-08-13 12:56:58 +0900

    추가

---
+++ Employee01/Unit1.pas
@@ -0,0 +1,52 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.ListView.Types, Data.Bind.GenData, Fmx.Bind.GenData, System.Rtti,
+  System.Bindings.Outputs, Fmx.Bind.Editors, Data.Bind.EngExt,
+  Fmx.Bind.DBEngExt, Data.Bind.Components, FMX.Objects, FMX.ListBox,
+  FMX.Layouts, FMX.StdCtrls, Data.Bind.ObjectScope, FMX.ListView,
+  FMX.Controls.Presentation, FMX.TabControl;
+
+type
+  TForm1 = class(TForm)
+    TabControl1: TTabControl;
+    TabItem1: TTabItem;
+    TabItem2: TTabItem;
+    ToolBar1: TToolBar;
+    Label1: TLabel;
+    ListView1: TListView;
+    PrototypeBindSource1: TPrototypeBindSource;
+    BindingsList1: TBindingsList;
+    LinkListControlToField1: TLinkListControlToField;
+    ToolBar2: TToolBar;
+    Label2: TLabel;
+    Button1: TButton;
+    ListBox1: TListBox;
+    ListBoxGroupHeader1: TListBoxGroupHeader;
+    ListBoxItem1: TListBoxItem;
+    ListBoxItem2: TListBoxItem;
+    ListBoxItem3: TListBoxItem;
+    Image1: TImage;
+    Label3: TLabel;
+    Label4: TLabel;
+    LinkPropertyToFieldBitmap: TLinkPropertyToField;
+    LinkPropertyToFieldText: TLinkPropertyToField;
+    LinkPropertyToFieldText2: TLinkPropertyToField;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.fmx}
+
+end.
